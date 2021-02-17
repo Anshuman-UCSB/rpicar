b12942c1389f69875f88a0b2d08ec8781ad4b2cb
Anshuman Dash <Anshuman@ucsb.edu>
2021-02-17 13:32:20 -0800

    test

--- gpiotest.py
+++ gpiotest.py
@@ -1,21 +1,8 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)
+led = LED(22)
+led.on()
 
-pins = {
-    22 : {'name' : 'GPIO 22', 'state' : GPIO.LOW},
-    23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
-    17 : {'name' : 'GPIO 17', 'state' : GPIO.LOW},
-    27 : {'name' : 'GPIO 27', 'state' : GPIO.LOW}
-}
-
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
-
-GPIO.output(23, GPIO.HIGH)
 sleep(2)
-GPIO.output(23, GPIO.LOW)
-
-GPIO.cleanup()
+led.off()
