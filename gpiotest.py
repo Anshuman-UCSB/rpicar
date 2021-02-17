ffb84d37b5ed011ee48f3a4cd1c15e775dd3e9ef
Anshuman Dash <Anshuman@ucsb.edu>
2021-02-17 13:18:24 -0800

    tester

---
+++ gpiotest.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+pins = {
+    22 : {'name' : 'GPIO 22', 'state' : GPIO.LOW},
+    23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
+    17 : {'name' : 'GPIO 17', 'state' : GPIO.LOW},
+    27 : {'name' : 'GPIO 27', 'state' : GPIO.LOW}
+}
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
+pins[22]['state'] =GPIO.HIGH
+sleep(2)
+pins[22]['state'] =GPIO.LOW
+
