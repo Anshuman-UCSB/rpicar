175ff47cc8c36dd999ec85619d62c039d0a9fe7e
Anshuman Dash <Anshuman@ucsb.edu>
2021-02-17 13:51:04 -0800

    lfkjas;dkfja

--- gpiotest.py
+++ gpiotest.py
@@ -1,9 +1,16 @@
+print(1)
 from gpiozero import LED
+print(2)
 from time import sleep
+print(3)
 
-led = LED(22)
+led = LED(17)
+print(4)
 led.on()
+print(5)
 
 sleep(2)
+print(6)
 led.off()
+print(7)
 
